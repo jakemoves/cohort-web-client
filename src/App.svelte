a49557ec87707cd2c5dc1375b63a74905c6dbe86
Jacob Niedzwiecki <jake@jacob-n.com>
2020-09-30 14:35:37 -0600

    Broke out component

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,5 @@
 <script>
-let audioUrl = "." + "/"/*window.location.pathname*/ + 'sounds/intro-score.mp3'
-console.log(audioUrl)
-let isPaused = true
-let isLoaded
-let audioPlayer, playButton
-let audioPreloadSetting = "auto"
-let audioDuration
-
-$: state = isPaused ? "paused" : "playing"
-$: isLoaded = (audioDuration !== undefined && !isNaN(audioDuration)) ? true : false
-
-
-function onBtnPlay() {
-  isPaused = false
-}
-
-function onBtnPause() {
-  isPaused = true
-}
+	import AudioPlayer from './AudioPlayer.svelte'
 </script>
 
-{#if isPaused}
-	<button 
-		type="button" 
-		on:click={onBtnPlay} 
-		class="btn btn-outline btn-outline-success btn-block"
-		bind:this={playButton}>
-		Play
-	</button>
-{:else}
-	<button 
-		type="button" 
-		on:click={onBtnPause} 
-		class="btn btn-outline btn-outline-warning btn-block">
-		{#if isLoaded}Pause{:else}Loading{/if}
-	</button>
-{/if}
-
-<audio 
-  id="audio_player" 
-  src={audioUrl}
-  preload={audioPreloadSetting}
-  bind:paused={isPaused}
-  bind:this={audioPlayer}
-  bind:duration={audioDuration}
-></audio>
+<AudioPlayer audioUrl={"." + "/" + 'sounds/intro-score.mp3'}/>
